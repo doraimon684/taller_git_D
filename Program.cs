a7236266a039715d9f65a5f58d598b67c3e01999
doraimon684 <tcm0032478@est.univalle.edu>
2023-05-10 16:09:33 -0400

    primer comit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace git_ejercicio
+{
+    internal class Program
+    {
+        void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int resutl = Sum(2, 3);
+        }
+       
+    }
+    public int Sum(int n1,int n2)
+    {
+        return n1 + n2;
+    }
+}
